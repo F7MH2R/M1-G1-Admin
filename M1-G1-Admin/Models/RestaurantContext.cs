b37df4582120facb1cdcacf5486c901f6618ab61
F7MH2R <skyicef7@gmail.com>
2024-04-14 17:38:01 -0600

    ok

--- M1-G1-Admin/Models/RestaurantContext.cs
+++ M1-G1-Admin/Models/RestaurantContext.cs
@@ -2,13 +2,21 @@
 
 namespace M1_G1_Admin.Models
 {
-    public class RestaurantContext:DbContext
+    public class RestaurantContext : DbContext
     {
-        public RestaurantContext(DbContextOptions<RestaurantContext> options): base (options)
+        public RestaurantContext(DbContextOptions<RestaurantContext> options) : base(options)
         {
 
 
         }
+        public DbSet<Cargo> cargo { get; set; }
+        public DbSet<Combos> combos { get; set; } 
+        public DbSet<Categoria_Platos> categorias_platos { get; set; }
+        public DbSet<Mesa> mesa { get; set; }
+        public  DbSet<Platos> platos { get; set; }
+        public DbSet<Promociones> promociones { get; set;}
+        public DbSet<Estado> estado { get; set; }
+        public DbSet<Empleado> empleados { get; set; }
 
     }
 }
