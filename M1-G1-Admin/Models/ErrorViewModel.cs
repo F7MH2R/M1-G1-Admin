43c69f41f830b04c8d636d23742b5ce793b0bb77
F7MH2R <skyicef7@gmail.com>
2024-04-14 15:55:25 -0600

    Add project files.

---
+++ M1-G1-Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace M1_G1_Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
